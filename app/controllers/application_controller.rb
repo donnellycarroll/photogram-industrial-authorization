6608b283ab6c1d5f098b9b1d3e9da16e41336473
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2025-04-15 20:37:32 +0000

    pundit broke everything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
-    # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-    allow_browser versions: :modern
+  include Pundit::Authorization
+  
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
 
   before_action :authenticate_user!
   before_action :set_user_search, if: -> { current_user.present? }
@@ -13,15 +15,17 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :private, :name, :bio, :website, :avatar_image ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :username, :private, :name, :bio, :website, :avatar_image ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :private, :name, :bio, :website, :avatar_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :private, :name, :bio, :website, :avatar_image])
   end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
 
-    redirect_back(fallback_location: root_url)
+    redirect_back fallback_location: root_url
   end
 end
---
+++ app/policies/photo_policy.rb
@@ -0,0 +1,3 @@
+class PhotoPolicy < ApplicationPolicy
+
+end

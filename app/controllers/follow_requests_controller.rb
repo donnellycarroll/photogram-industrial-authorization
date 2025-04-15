8e86407b13ec1281691a8f03a1548f25f3f53d8b
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2025-04-15 19:50:55 +0000

    cleaned up grade a little still can't pass all

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -67,4 +67,5 @@ class FollowRequestsController < ApplicationController
     def follow_request_params
       params.require(:follow_request).permit(:recipient_id, :sender_id, :status)
     end
+    
 end
